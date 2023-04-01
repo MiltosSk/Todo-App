9fa14d282483bc0ec871833d9b31d02f2eb9d283
Miltos Skondras <miltiadis.skondras.2018@gmail.com>
2023-04-01 10:05:24 +0300

    Initial showTodo function

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ taskBox = document.querySelector(".task-box")
 let editId,
 isEditTask = false,
 todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach(btn =>{
+    btn.addEventListener("click", () =>{
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    })
+});
+
+function showTodo(filter){
+    let liTag = "";
+    if(todos){
+        todos.forEach((todo, id) =>{
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all"){
+                liTag += `<li class="task">
+                    <label for="${id}">
+                        <input onClick="updateStatus(this)"
+                        type="checkbox" id="${id}" ${completed}>
+                        <p class="${completed}">${todo.name}</p>
+                    </label>
+                    `
+            }
+        })
+    }
+}
