ee01735727d104e894d4f561ae613015ef1e0fba
Miltos Skondras <miltiadis.skondras.2018@gmail.com>
2023-04-03 22:43:36 +0300

    Adding showMenu function

--- index.js
+++ index.js
@@ -28,21 +28,31 @@ function showTodo(filter){
                         <p class="${completed}">${todo.name}</p>
                     </label>
                     <div class="settings">
-                        <i onClick="showMenu(this)" class="uil uil-ellipsis-h></i>
+                        <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
                         <ul class="task-menu">
-                            <li onClick='edit-task(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                            <li onClick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                            <li onclick='edit-task(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                            <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
                         </ul>
                     </div>
-                </li>`
+                </li>`;
             }
-        })
+        });
     }
 
 
     taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-    let checkTask = taskBox.querySelector(".task");
+    let checkTask = taskBox.querySelectorAll(".task");
     !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
     taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
 }
 showTodo("all");
+
+function showMenu(selectedTask){
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e =>{
+        if(e.target.tagName != "I" || e.target != selectedTask){
+            menuDiv.classList.remove("show");
+        }
+    });
+}
